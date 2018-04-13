67e0cbc7a493b558d68f375d4d87ebdedcff93be
Zbyněk Hanzlík <Anub.W@seznam.cz>
2018-04-13 17:35:26 +0200

    pridany polozky src/account.cpp a include/account.hpp plus v *.hpp definice tridy

---
+++ C10_Test_05/account.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <string>
+class Account
+{
+private:
+	std::string jmeno;
+	long cisloUctu;
+	float zustatek;
+
+public:
+	Account(const std::string s = "null", long an = -1, float bal = 0.0);
+	~Account();
+
+	void ZobrazZustatek() const;
+	void Vybrat(float amt);
+	void Vlozit(float amt);
+	float Zustatek() const;
+
+};
+
